e1cebd6e1a9822afd4c8d8eefe7484af090378e5
Mahan <131691001+mahanij@users.noreply.github.com>
2024-01-06 15:26:13 +0330

    version-1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "syne":['Syne, sans-serif'],
+      "rubik":['Rubik, sans-serif'],
+    },
+
+    extend: {
+      colors: {
+        "primary":"#FF9143",
+        "secondry":{
+          100:'#1A1A1A',
+          200:'#262A2C',
+          300:'#696969',
+          400:'#FFFFFF',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
